fe7834aabebf57114d41aacdf2586abdb667688e
Nathan S <nathan.starkweather@gmail.com>
2016-03-07 10:57:15 -0800

    Merge branch 'master' of github.com:nitetrain8/scripts
    Conflicts:
    	pycharm_startup_del_path0.py

--- const/__init__.py
+++ const/__init__.py
@@ -8,8 +8,9 @@ Created in: PyCharm Community Edition
 """
 __author__ = 'Nathan Starkweather'
 
-from .const import *
+from .xlconst import *
 from .shellconst import *
+from .wdconst import *
 
 if __name__ == "__main__":
     pass
