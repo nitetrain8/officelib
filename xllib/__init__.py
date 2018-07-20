c43e60c43545af28ec1e441c9785726099a977c2
Nathan Starkweather <nstarkweather@pbsbiotech.com>
2018-07-19 19:00:20 -0700

    auto backup

--- xllib/__init__.py
+++ xllib/__init__.py
@@ -12,3 +12,16 @@ from .xlcom import *
 from .xladdress import *
 from . import xlcom
 from . import xladdress
+import win32com
+import win32com.client
+
+class Constants():
+    def __init__(self):
+        pass
+
+    def __getattr__(self, attr):
+        val = getattr(win32com.client.constants, attr)
+        setattr(self, attr, val)
+        return val
+
+xlc = Constants()
