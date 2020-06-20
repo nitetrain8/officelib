f3c63ed0aaed70b6b1a8aff89b164905bdb0b686
Nathan Starkweather <nathan.starkweather@gmail.com>
2020-06-20 12:24:24 -0700

    auto backup

--- xllib/__init__.py
+++ xllib/__init__.py
@@ -17,7 +17,12 @@ import win32com.client
 
 class Constants():
     def __init__(self):
-        pass
+        # based on win32com.client.constants.__getattr__
+        c = win32com.client.constants
+        for d in c.__dicts__:
+            for k in d:
+                if k.startswith("xl"):
+                    setattr(self, k, d[k])
 
     def __getattr__(self, attr):
         val = getattr(win32com.client.constants, attr)
